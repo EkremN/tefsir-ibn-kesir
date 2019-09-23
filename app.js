6c23b9e913c1eb4e5e42fe32339487b434ebfadc
Ekrem Nurovic <ekrem.nurovic@gmail.com>
2019-09-23 23:25:24 +0200

    Fixed email II

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
+// let template_params = {
+//    "name": "name_value",
+//    "surname": "surname_value",
+//    "email": "email_value",
+//    "adress": "adress_value",
+//    "city": "city_value",
+//    "country": "country_value",
+//    "phone_number": "phone_number_value"
+// }
+
 function sendMesage() {
   document.getElementById('contact-form').addEventListener('submit', function(event) {
       event.preventDefault();
-      emailjs.send("kontakt_servis", "tefsir_inb_kesir_forma", this)
+      emailjs.sendForm("kontakt_servis", "tefsir_inb_kesir_forma", this )
     });
 }
 
--- script.js
+++ script.js
@@ -1,27 +1,58 @@
-const inputs = document.querySelectorAll("input")
-const notification = document.getElementById("notification")
-const form = document.getElementById("contact-form")
-let formInput;
+// const inputs = document.querySelectorAll("input")
+// const notification = document.getElementById("notification")
+// const form = document.getElementById("contact-form")
+// let formInput;
 
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    inputs.forEach(input => {
+// form.addEventListener("submit", (e) => {
+//     e.preventDefault();
+//     inputs.forEach(input => {
 
-        if (input.value == "") {
-            input.classList.add("red-border")
-            formInput = false;
-        } else {
-            input.classList.remove("red-border")
-            formInput = true;
-        }
+//         if (input.value == "") {
+//             input.classList.add("red-border")
+//             formInput = false;
+//         } else {
+//             input.classList.remove("red-border")
+//             formInput = true;
+//         }
 
-    })
+//     })
 
-    if (formInput) {
-        alertify.success("Pretplata poslana, hvala na povjerenju")
-        inputs.forEach(input => input.value = "")
-        sendMesage()
+//     if (formInput) {
+//         alertify.success("Pretplata poslana, hvala na povjerenju")
+
+//         sendMesage()
+//         // window.location.reload();
+//         // inputs.forEach(input => input.value = "")
+//     } else {
+//         alertify.error("Molimo vas provjerite prijavu");
+//     }
+// })
+
+$(document).ready(function() {
+  $('#contact-form').submit(function(e) {
+    let name    = document.getElementById('inputName'),
+        surname = document.getElementById('surname'),
+        email   = document.getElementById('inputEmail'),
+        city = document.getElementById('city'),
+        address = document.getElementById('address'),
+        country = document.getElementById('country'),
+        adress = document.getElementById('adress'),
+        phone_number = document.getElementById('phone_number')
+
+
+    if (!name.value || !email.value || !surname.value || !city.value || !address.value || !county.value || !phone_number.value) {
+      alertify.error("Molimo vas provjerite formu");
+      return false;
     } else {
-        alertify.error("Molimo vas provjerite prijavu");
+      $.ajax({
+        method: 'POST',
+        url: '//formspree.io/elkelimeh.bosna@gmail.com',
+        data: $('#contact-form').serialize(),
+        datatype: 'json'
+      });
+      e.preventDefault();
+      $(this).get(0).reset();
+      alertify.success("Prijava za pretplatu poslana");
     }
-})
+  });
+});
